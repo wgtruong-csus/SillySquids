274aef565ac740209907b6bd16e2748dd1fcacb3
ryan <ryan@localhost>
2022-02-21 15:00:59 -0800

    Contribution info

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("219114650 - This line was added by Waih Truong.");
 		System.out.println("218728771 - This line was added by Tyler Rice.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("218963889 - This line was added by Ryan Si.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
