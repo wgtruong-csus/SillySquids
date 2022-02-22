ebff0412dbf3d8fdcf1fd981066488e7315f8a34
BodikH <99681981+BodikH@users.noreply.github.com>
2022-02-21 16:58:32 -0800

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
 		System.out.println("218963889 - This line was added by Ryan Si .");
 		System.out.println("219339667 - This line was added by Ikjot Shergill.");
 		System.out.println("220053185 - This line was added by Sukhdeep Banwait.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by Bohdan Hrytsak.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
